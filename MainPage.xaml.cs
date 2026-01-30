ef5e90d07093c12ad051f2de6748d816edbfd6d9
Lucas Coelho <lucascoelhosi96@gmail.com>
2026-01-30 11:14:50 -0300

    feat: customizacao de exibicao e finalizacao da logica do empate no jogo da velha

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,148 +2,101 @@
 {
     public partial class MainPage : ContentPage
     {
+        Button[,] tabuleiro;
         string vez = "X";
 
         public MainPage()
         {
             InitializeComponent();
+
+            tabuleiro = new Button[3, 3]
+            {
+                { btn20, btn21, btn22 },
+                { btn30, btn31, btn32 },
+                { btn40, btn41, btn42 }
+            };
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
 
+            btn.Text = vez;
             btn.IsEnabled = false;
 
-            /* Verificar qual letra preencher no botão */
-            if (vez == "X")
+            // Cor automática
+            btn.TextColor = CorDoJogador(vez);
+
+            if (VerificarVencedor(vez))
             {
-                btn.Text = "X";
-                label_vez.Text = "Vez do O jogar";
-                vez = "O";
-            } else
-            {
-                btn.Text = "O";
-                label_vez.Text = "Vez do X jogar";
-                vez = "X";
+                DisplayAlertAsync("Parabéns!", $"O jogador {vez} venceu!", "OK");
+                Zerar();
+                return;
             }
 
-            /* Verificar se o X ganhou nas linhas */
-            if (btn20.Text == "X" && btn21.Text == "X" && btn22.Text == "X")
+            if (VerificarEmpate())
             {
-                DisplayAlertAsync("Parabéns!", "O jogador X ganhou na linha 1!", "ok");
+                DisplayAlertAsync("Deu velha!", "Ninguém venceu 😅", "OK");
                 Zerar();
+                return;
             }
 
-            if (btn30.Text == "X" && btn31.Text == "X" && btn32.Text == "X")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador X ganhou na linha 2!", "ok");
-                Zerar();
-            }
-
-            if (btn40.Text == "X" && btn41.Text == "X" && btn42.Text == "X")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador X ganhou na linha 3!", "ok");
-                Zerar();
-            }
-
-            /* Verificar se o X ganhou nas colunas */
-            if (btn20.Text == "X" && btn30.Text == "X" && btn40.Text == "X")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador X ganhou na coluna 1!", "ok");
-                Zerar();
-            }
-
-            if (btn21.Text == "X" && btn31.Text == "X" && btn41.Text == "X")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador X ganhou na coluna 2!", "ok");
-                Zerar();
-            }
-
-            if (btn22.Text == "X" && btn32.Text == "X" && btn42.Text == "X")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador X ganhou na coluna 3!", "ok");
-                Zerar();
-            }
-
-            /* Verificar se o X ganhou nas diagonais */
-            if (btn20.Text == "X" && btn31.Text == "X" && btn42.Text == "X")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador X ganhou na diagonal 1!", "ok");
-                Zerar();
-            }
-
-            if (btn22.Text == "X" && btn31.Text == "X" && btn40.Text == "X")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador X ganhou na diagonal 2!", "ok");
-                Zerar();
-            }
-
-            /* Verificar se o O ganhou nas linhas */
-            if (btn20.Text == "O" && btn21.Text == "O" && btn22.Text == "O")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador O ganhou na linha 1!", "ok");
-                Zerar();
-            }
-
-            if (btn30.Text == "O" && btn31.Text == "O" && btn32.Text == "O")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador O ganhou na linha 2!", "ok");
-                Zerar();
-            }
-
-            if (btn40.Text == "O" && btn41.Text == "O" && btn42.Text == "O")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador O ganhou na linha 3!", "ok");
-                Zerar();
-            }
-
-            /* Verificar se o O ganhou nas colunas */
-            if (btn20.Text == "O" && btn30.Text == "O" && btn40.Text == "O")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador O ganhou na coluna 1!", "ok");
-                Zerar();
-            }
-
-            if (btn21.Text == "O" && btn31.Text == "O" && btn41.Text == "O")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador O ganhou na coluna 2!", "ok");
-                Zerar();
-            }
-
-            if (btn22.Text == "O" && btn32.Text == "O" && btn42.Text == "O")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador O ganhou na coluna 3!", "ok");
-                Zerar();
-            }
-
-            /* Verificar se o O ganhou nas diagonais */
-            if (btn20.Text == "O" && btn31.Text == "O" && btn42.Text == "O")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador O ganhou na diagonal 1!", "ok");
-                Zerar();
-            }
-
-            if (btn22.Text == "O" && btn31.Text == "O" && btn40.Text == "O")
-            {
-                DisplayAlertAsync("Parabéns!", "O jogador O ganhou na diagonal 2!", "ok");
-                Zerar();
-            }
+            // Troca de jogador
+            vez = vez == "X" ? "O" : "X";
+            label_vez.Text = $"Vez do {vez} jogar";
         }
+
+        bool VerificarVencedor(string jogador)
+        {
+            // Linhas
+            for (int i = 0; i < 3; i++)
+                if (tabuleiro[i, 0].Text == jogador &&
+                    tabuleiro[i, 1].Text == jogador &&
+                    tabuleiro[i, 2].Text == jogador)
+                    return true;
+
+            // Colunas
+            for (int j = 0; j < 3; j++)
+                if (tabuleiro[0, j].Text == jogador &&
+                    tabuleiro[1, j].Text == jogador &&
+                    tabuleiro[2, j].Text == jogador)
+                    return true;
+
+            // Diagonais
+            if (tabuleiro[0, 0].Text == jogador &&
+                tabuleiro[1, 1].Text == jogador &&
+                tabuleiro[2, 2].Text == jogador)
+                return true;
+
+            if (tabuleiro[0, 2].Text == jogador &&
+                tabuleiro[1, 1].Text == jogador &&
+                tabuleiro[2, 0].Text == jogador)
+                return true;
+
+            return false;
+        }
+
+        bool VerificarEmpate()
+        {
+            return tabuleiro
+                .Cast<Button>()
+                .All(b => !string.IsNullOrEmpty(b.Text));
+        }
+
 
         void Zerar()
         {
-            foreach (View i in ((Grid)this.Content).Children.Where(x => x.GetType() == typeof(Button)))
+            foreach (var btn in tabuleiro)
             {
-                {
-                    if (i.GetType() == typeof(Button))
-                    {
-                        Button btn = (Button)i;
-                        btn.Text = "";
-                        btn.IsEnabled = true;
-                    }
-                }
+                btn.Text = "";
+                btn.IsEnabled = true;
             }
+
+            vez = "X";
+            label_vez.Text = "Vez do X jogar";
         }
+
+        Color CorDoJogador(string jogador)
+            => jogador == "X" ? Colors.DodgerBlue : Colors.Orange;
     }
 }
